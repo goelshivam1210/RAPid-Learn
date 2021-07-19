cdd30d08218c2367d7c662264d6e6832f8f33381
shukla-yash <yshukla@wpi.edu>
2021-07-19 19:05:17 -0400

    Updated problem pddl generation

--- generate_pddl.py
+++ generate_pddl.py
@@ -37,90 +37,35 @@ def _generate_header_prob():
 
 def _generate_objects(env):
     objects_list = []
-    for i in range(env.map_size):
-        for j in range(1,env.map_size):
-            objects_list.append("\t\tloc_"+str(i)+"_"+str(j)+" - world")
-    objects_list.append("\t\tnorth - direction")
-    objects_list.append("\t\tsouth - direction")
-    objects_list.append("\t\teast - direction")
-    objects_list.append("\t\twest - direction")
-    objects_list.append("\t\tself - actor")
-    objects_list.append("\t\tnothing - material")
     for item in env.items:
-        objects_list.append("\t\t"+item+" - material")
+        objects_list.append("\t\t"+item+" - "+item)
     objs = "\n".join(objects_list)
     return "\n".join(["\t(:objects", objs, "\t)"])
 
 def _generate_init(env):
     init_list = []
-    for i in range(1,env.map_size):
-        for j in range(1, env.map_size):
-            init_list.append("\t\t(adjacent loc_"+str(i)+"_"+str(j)+" loc_"+str(i-1)+"_"+str(j)+ " north)")
-    for i in range(env.map_size-1):
-        for j in range(1, env.map_size):
-            init_list.append("\t\t(adjacent loc_"+str(i)+"_"+str(j)+" loc_"+str(i+1)+"_"+str(j)+ " south)")
-    for i in range(env.map_size):
-        for j in range(1,env.map_size-1):
-            init_list.append("\t\t(adjacent loc_"+str(i)+"_"+str(j+1)+" loc_"+str(i)+"_"+str(j)+ " west)")
-    for i in range(env.map_size):
-        for j in range(1,env.map_size-1):
-            init_list.append("\t\t(adjacent loc_"+str(i)+"_"+str(j)+" loc_"+str(i)+"_"+str(j+1)+ " east)")
-    init_list.append("\t\t(opposite north south)")
-    init_list.append("\t\t(opposite south north)")
-    init_list.append("\t\t(opposite east west)")
-    init_list.append("\t\t(opposite west east)")
-    init_list.append("\t\t(clockwise north east)")
-    init_list.append("\t\t(clockwise south west)")
-    init_list.append("\t\t(clockwise east south)")
-    init_list.append("\t\t(clockwise west north)")
 
-    for i in range(env.map_size):
-        init_list.append("\t\t(isBlock wall loc_"+str(i)+"_"+str(env.map_size-1)+")")
-        init_list.append("\t\t(at wall loc_"+str(i)+"_"+str(env.map_size-1)+")")
-    for i in range(1,env.map_size-1):
-        init_list.append("\t\t(isBlock wall loc_"+str(env.map_size-1)+"_"+str(i)+")")        
-        init_list.append("\t\t(at wall loc_"+str(env.map_size-1)+"_"+str(i)+")")        
-    for i in range(1,env.map_size-1):
-        init_list.append("\t\t(isBlock wall loc_0_"+str(i)+")")
-        init_list.append("\t\t(at wall loc_0_"+str(i)+")")
+    wall_size = env.map_size*4-4
+
+    air_size = env.map_size*env.map_size - wall_size - sum(env.items_quantity[item] for item in env.items_quantity)
 
     for item in env.items:
         init_list.append("\t\t(= (inventory "+str(item)+") "+str(env.inventory_items_quantity[item])+")")
-    init_list.append("\t\t(= ( inventory nothing) 1)")
+        if item in env.items_quantity:
+            init_list.append("\t\t(= (world "+str(item)+") "+str(env.items_quantity[item])+")")
+        elif item == 'wall':
+            init_list.append("\t\t(= (world wall) "+str(wall_size)+")")
+        elif item == 'air':
+            init_list.append("\t\t(= (world air) "+str(air_size)+")")
+        else:
+            init_list.append("\t\t(= (world "+str(item)+") "+"0"+")")
 
-    for i in range(1,env.map_size-1):
-        for j in range(1,env.map_size-1):
-            if env.map[i][j] == 0:
-                init_list.append("\t\t(isBlock air loc_"+str(i)+"_"+str(j)+")")
-                init_list.append("\t\t(at air loc_"+str(i)+"_"+str(j)+")")
-            else:
-                init_list.append("\t\t(isBlock "+ list(env.items_id.keys())[list(env.items_id.values()).index(env.map[i][j])]+" loc_"+str(i)+"_"+str(j)+")")
-                init_list.append("\t\t(at "+ list(env.items_id.keys())[list(env.items_id.values()).index(env.map[i][j])]+" loc_"+str(i)+"_"+str(j)+")")
+    if env.selected_item == '':
+        init_list.append("\t\t(holding air)")
+    else:
+        init_list.append("\t\t(holding "+env.selected_item+")")
 
-    for item in env.unbreakable_items:
-        init_list.append("\t\t(unbreakable "+item+")")
-    init_list.append("\t\t(unbreakable tree_tap)")
-
-    init_list.append("\t\t(tapper tree_tap)")
-    init_list.append("\t\t(tapout rubber)")
-    init_list.append('\t\t(craftsticksouta stick)')
-    init_list.append('\t\t(permeable air)')
-    init_list.append('\t\t(craftpogostickinc rubber)')
-    init_list.append('\t\t(craftplanksina tree_log)')
-    init_list.append('\t\t(craftsticksina plank)')
-    init_list.append('\t\t(crafttreetapinb plank)')
-    init_list.append('\t\t(crafttreetapina stick)')
-    init_list.append('\t\t(crafttreetapouta tree_tap)')
-    init_list.append('\t\t(craftpogostickouta pogo_stick)')
-    init_list.append('\t\t(craftpogostickinb plank)')
-    init_list.append('\t\t(craftpogostickina stick)')
-    init_list.append('\t\t(holding nothing)')
-    init_list.append('\t\t(crafter crafting_table)')
-    init_list.append('\t\t(craftplanksouta plank)')
-    init_list.append('\t\t(tappable tree_log)')
-
-    init_list.append("\t\t(at self loc_"+str(env.agent_location[0])+"_"+str(env.agent_location[1])+")")
-    init_list.append("\t\t(orientation self "+str(env.agent_facing_str).lower()+")")
+    init_list.append("\t\t(near crafting_table)")
 
     ints = "\n".join(init_list)
     return "\n".join(["\t(:init", ints, "\t)"])
