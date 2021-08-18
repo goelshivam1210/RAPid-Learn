487e97c964a86b76dc5d17495816f69c5ecad2f1
goelshivam1210 <goelshivam1210@gmail.com>
2021-08-18 18:37:24 -0400

    renoved clutter

--- envs/__init__.py
+++
@@ -1,7 +0,0 @@
-# from rl_agent.dqn_lambda.envs.novelgridworld_standalone.novelgridworld_env import NovelGridworldV7Env
-# from gym.envs.registration import register
-#
-# register(
-#     id='NovelGridworld-v7',
-#     entry_point='polycraft_tufts.rl_agent.dqn_lambda.envs.novelgridworld_env:NovelGridworldV7Env',
-# )
--- envs/novelgridworld_standalone/__init__.py
+++
@@ -1,7 +0,0 @@
-from polycraft_tufts.rl_agent.dqn_lambda.envs.novelgridworld_standalone.pogostick_v1_env import PogostickV2Env
-from gym.envs.registration import register
-
-register(
-    id='NovelGridworld-Pogostick-v2',
-    entry_point='polycraft_tufts.rl_agent.dqn_lambda.envs.novelgridworld_standalone.pogostick_v1_env:PogostickV2Env',
-)
